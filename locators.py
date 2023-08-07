0768f2aee51dd8d65367f6c9fdea1daed59c4ad8
carlosrosendo <129891840+carlosrosendo@users.noreply.github.com>
2023-08-07 15:44:52 -0700

    locators

--- locators.py
+++ locators.py
@@ -1,6 +1,9 @@
+from select import select
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -18,6 +21,12 @@ driver.find_element(By.CSS_SELECTOR,"input[value='option1'")
 
 driver.find_element(By.CSS_SELECTOR,"input[name='name'").send_keys("TestUser")
 driver.find_element(By.CSS_SELECTOR,"#inlineRadio1").click()
+
+#Static drop down
+dropdown = Select(driver.find_element(By.ID, "exampleFormControlSelect1"))
+dropdown.select_by_visible_text("Female")
+dropdown.select_by_index(0)
+#dropdown.select_by_value("teach")
 driver.find_element(By.XPATH, "//input[@type='submit']").click()
 message = driver.find_element(By.CLASS_NAME, "alert-success").text
 print(message)
